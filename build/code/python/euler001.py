4409c3819f767e160e8287d34fb7e00aca5cac34
Steve Lawford <steve_lawford@yahoo.co.uk>
2016-10-24 14:01:34 +0200

    Correct error in condition

--- build/code/python/euler001.py
+++ build/code/python/euler001.py
@@ -7,9 +7,9 @@ def add_natural(n):
 
     sum = 0
     
-    for i in range(1, n + 1):
+    for i in range(1, n):
         
-        if (i % 3) and (i % 5):
+        if (i % 3) == 0 or (i % 5) == 0:
             
             sum += i
         
